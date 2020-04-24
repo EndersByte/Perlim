beffbf7da2b8e534e368508b2b3ead4740b6dc87
Filipe "Ender" Araujo <death.enderman@gmail.com>
2020-04-24 09:46:56 -0300

    zero dimentions - moviment across a line

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let xoff = 0
+
 function setup() {
-	createCanvas(1080, 720);
+	//createCanvas(1080, 720)
+	createCanvas(500, 300)
 }
 
 function draw() {
-
+	background(51)
+	//let x = random(width)
+	let x = map(noise(xoff), 0, 1, 0, width)
+	xoff += 0.03
+	ellipse(x, 200, 24)
 }
