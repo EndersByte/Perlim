5f29bbcb0b377ade66327feace56a4fd0e6ede8c
Filipe "Ender" Araujo <death.enderman@gmail.com>
2020-04-29 20:06:41 -0300

    Noise led grid

--- sketch.js
+++ sketch.js
@@ -1,29 +1,29 @@
-let inc = 0.01
-let scl = 20
+let inc = 0.2
+let scl = 5
 let cols, rows
+let yoff, xoff
 
 function setup() {
-	createCanvas(300, 300)
+	createCanvas(480, 360)
 	cols = floor(width / scl)
 	rows = floor(height / scl)
 }
 
 function draw() {
 	background(51)
-	
-	let yoff = 0
+
+	yoff +=2*inc
 	for (let y = 0; y < rows; y++) {
-		let xoff = 0
+		xoff +=2*inc
 		for (let x = 0; x < cols; x++) {
 			let index = (x + y * width) * 4
-			let r = random(255)
-			//let r = noise(xoff, yoff) * 255
-			fill (r)
-			rect(x*scl,y*scl,scl,scl)
+			//let r = random(255)
+			let r = noise(xoff, yoff) * 255
+			fill(r)
+			rect(x * scl, y * scl, scl, scl)
 			xoff += inc
 		}
 		yoff += inc
 	}
-	
 	//noLoop()
 }
