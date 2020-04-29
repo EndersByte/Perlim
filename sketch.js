cf003424464f239b24cd67ed32fdd7139cc14f17
Filipe "Ender" Araujo <death.enderman@gmail.com>
2020-04-29 18:07:02 -0300

    noise pixel value

--- sketch.js
+++ sketch.js
@@ -11,11 +11,13 @@ function draw() {
 	for (let y = 0; y < height; y++) {
 		for (let x = 0; x < width; x++) {
 			let index = (x + y * width) * 4
-			let r = random(255)
+			//let r = random(255)
+			let r = noise(xoff)*255
 			pixels[index] = r
 			pixels[index + 1] = r
 			pixels[index + 2] = r
 			pixels[index + 3] = 255
+			xoff+=inc
 		}
 	}
 	updatePixels()
